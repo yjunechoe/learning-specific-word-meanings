f30ee4c37e3cb37d067eeb0eb0057c2b28db9b46
June Choe <jchoe001@gmail.com>
2021-09-11 09:41:23 -0400

    randomize trial sequence template

--- pcibex/Experiment 1/main.js
+++ pcibex/Experiment 1/main.js
@@ -1,20 +1,38 @@
-PennController.ResetPrefix()
+PennController.ResetPrefix(null)
 
-newTrial(
-    newSelectionGrid("test-phase")
+PennController.Sequence("consent", randomize("experiment"))
+
+PennController( "consent" ,
+    defaultText
         .print()
-        .hide()
-        .log()
     ,
-    newTimer("hurry", 3000)
-        .start()
+    newText("<h2><b>CONSENT TO PARTICIPATE IN RESEARCH</b></h2>")
+        .settings.center()
+    ,
+    newButton("I Agree")
+        .settings.center()
+        .print()
         .wait()
-    ,
-    getSelectionGrid("test-phase")
-        .show()
-    ,
-    newButton("Continue")
-        .print()
-        .center()
-        .wait(getSelectionGrid("test-phase").test.selectAny())
+)
+
+Template("choice_set_df.csv", row => 
+    // learn-phase goes here before test-phase
+    newTrial("experiment",
+        newSelectionGrid("test-phase", row.choices)
+            .print()
+            .hide()
+            .log()
+        ,
+        newTimer("hurry", 3000)
+            .start()
+            .wait()
+        ,
+        getSelectionGrid("test-phase")
+            .show()
+        ,
+        newButton("Continue")
+            .print()
+            .center()
+            .wait(getSelectionGrid("test-phase").test.selectAny())
+    )
 )
